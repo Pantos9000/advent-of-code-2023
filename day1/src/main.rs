883d35dd03f9eaeafa6aeaa023f9e50b703d308a
Pantos <pantos+github@akk.org>
2023-12-05 16:19:02 +0100

    day1: refactor

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,4 @@
-#[allow(dead_code)]
-fn get_first_digit_old(line: &str, from_right: bool) -> Option<u32> {
+fn part1_get_first_digit(line: &str, from_right: bool) -> Option<u32> {
     let mut num_iter = line.chars().filter(|c| c.is_digit(10));
     let num = if from_right {
         num_iter.rev().next()?
@@ -7,6 +6,14 @@ fn get_first_digit_old(line: &str, from_right: bool) -> Option<u32> {
         num_iter.next()?
     };
     num.to_digit(10)
+}
+
+fn part1_get_num_from_line(line: &str) -> Option<u32> {
+    let num_left = part1_get_first_digit(line, false)?;
+    let num_right = part1_get_first_digit(line, true)?;
+
+    let num = 10 * num_left + num_right;
+    Some(num)
 }
 
 fn substring_to_num(substr: &str) -> Option<u32> {
@@ -60,11 +67,20 @@ fn read_input() -> String {
     fs::read_to_string(input_path).unwrap()
 }
 
+fn part1(input: &str) -> u32 {
+    input.lines().filter_map(part1_get_num_from_line).sum()
+}
+
+fn part2(input: &str) -> u32 {
+    input.lines().filter_map(get_num_from_line).sum()
+}
+
 fn main() {
     let input = read_input();
-    let sum: u32 = input.lines().filter_map(get_num_from_line).sum();
-
-    println!("Sum is {sum}");
+    let result1 = part1(&input);
+    let result2 = part2(&input);
+    println!("Part1 result is {result1}");
+    println!("Part2 result is {result2}");
 }
 
 #[cfg(test)]
