9606d5b02366532c2b8714856276fa35c677c5a5
Pantos <pantos+github@akk.org>
2023-12-16 18:15:07 +0100

    day9: add part2

--- day9/src/part2.rs
+++ day9/src/part2.rs
@@ -1,3 +1,27 @@
-pub fn run(_input: &str) -> usize {
-    0 // TODO
+use crate::part1;
+
+pub fn run(input: &str) -> isize {
+    input
+        .lines()
+        .map(|line| line.split(" ").map(|s| s.parse().unwrap()))
+        .map(calc_prev)
+        .sum()
+}
+
+fn calc_prev(history: impl Iterator<Item = isize>) -> isize {
+    part1::calc(history, part1::CalcWhat::Prev)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        let input = "\
+            0 3 6 9 12 15\n\
+            1 3 6 10 15 21\n\
+            10 13 16 21 30 45";
+        assert_eq!(run(input), 2);
+    }
 }
