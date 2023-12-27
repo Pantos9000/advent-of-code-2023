ca5be37ee3e864c934c09bff78369ab9f4250e87
Pantos <pantos+github@akk.org>
2023-12-27 19:29:34 +0100

    day11/part1: add expanding universe

--- day11/src/part1.rs
+++ day11/src/part1.rs
@@ -1,3 +1,126 @@
-pub fn run(_input: &str) -> usize {
+use std::str::FromStr;
+
+pub fn run(input: &str) -> usize {
+    let mut universe = Universe::from_str(input).unwrap();
+    universe.expand();
     todo!()
+}
+
+pub struct Galaxy {
+    x: usize,
+    y: usize,
+}
+
+impl Galaxy {
+    fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
+}
+
+pub struct Universe {
+    galaxies: Vec<Galaxy>,
+    size_x: usize,
+    size_y: usize,
+}
+
+impl Universe {
+    fn is_column_empty(&self, column: usize) -> bool {
+        self.galaxies
+            .iter()
+            .filter(|galaxy| galaxy.x == column)
+            .count()
+            == 0
+    }
+
+    fn is_row_empty(&self, row: usize) -> bool {
+        self.galaxies
+            .iter()
+            .filter(|galaxy| galaxy.y == row)
+            .count()
+            == 0
+    }
+
+    fn expand(&mut self) {
+        let mut rows_to_double = Vec::new();
+        let mut columns_to_double = Vec::new();
+
+        for x in 0..self.size_x {
+            if self.is_column_empty(x) {
+                columns_to_double.push(x);
+            }
+        }
+
+        for y in 0..self.size_y {
+            if self.is_row_empty(y) {
+                rows_to_double.push(y);
+            }
+        }
+
+        for galaxy in &mut self.galaxies {
+            let num_columns_doubled_before_galaxy =
+                columns_to_double.iter().filter(|x| **x < galaxy.x).count();
+            let num_rows_doubled_before_galaxy =
+                rows_to_double.iter().filter(|y| **y < galaxy.y).count();
+            galaxy.x += num_columns_doubled_before_galaxy;
+            galaxy.y += num_rows_doubled_before_galaxy;
+        }
+    }
+}
+
+impl FromStr for Universe {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        fn parse_line_with_num((y, line): (usize, &str)) -> impl Iterator<Item = Galaxy> + '_ {
+            line.chars()
+                .enumerate()
+                .filter(|(_x, c)| *c == '#')
+                .map(move |(x, _c)| Galaxy::new(x, y))
+        }
+
+        let galaxies: Vec<_> = input
+            .lines()
+            .enumerate()
+            .flat_map(parse_line_with_num)
+            .collect();
+
+        let size_x = input.lines().next().unwrap().len();
+        let size_y = input.lines().count();
+
+        Ok(Self {
+            galaxies,
+            size_x,
+            size_y,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_universe_expand() {
+        let input = "\
+            .#.#\n\
+            ....\n\
+            .#..\n\
+            ....\n\
+            #...";
+        let mut universe = Universe::from_str(input).unwrap();
+        universe.expand();
+
+        let galaxy = universe.galaxies.pop().unwrap();
+        assert_eq!(galaxy.x, 0);
+        assert_eq!(galaxy.y, 6);
+        let galaxy = universe.galaxies.pop().unwrap();
+        assert_eq!(galaxy.x, 1);
+        assert_eq!(galaxy.y, 3);
+        let galaxy = universe.galaxies.pop().unwrap();
+        assert_eq!(galaxy.x, 4);
+        assert_eq!(galaxy.y, 0);
+        let galaxy = universe.galaxies.pop().unwrap();
+        assert_eq!(galaxy.x, 1);
+        assert_eq!(galaxy.y, 0);
+    }
 }
