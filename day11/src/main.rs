8737811709af4e4e74e48fc7900949f1a29b0c90
Pantos <pantos+github@akk.org>
2023-12-27 17:13:59 +0100

    day11: add skeleton

---
+++ day11/src/main.rs
@@ -0,0 +1,27 @@
+mod part1;
+mod part2;
+
+pub fn read_input() -> String {
+    use std::fs;
+
+    let input_path = "./input";
+    fs::read_to_string(input_path).unwrap()
+}
+
+fn main() {
+    let input = read_input();
+    let result1 = part1::run(&input);
+    let result2 = part2::run(&input);
+    println!("Result1 is {result1}");
+    println!("Result2 is {result2}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1_solution() {
+        // TODO
+    }
+}
