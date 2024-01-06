ad83fc1c2f94029dd23154c48fe92f6b66849ced
Pantos <pantos+github@akk.org>
2024-01-06 12:04:34 +0100

    day12/part2: move part1-specific tests

--- day12/src/part1.rs
+++ day12/src/part1.rs
@@ -393,4 +393,52 @@ mod tests {
         let line = "?###???????? 3,2,1";
         assert_eq!(count_per_line(line), 10);
     }
+
+    #[test]
+    fn test_unfold_example_1_springs() {
+        let line = ".# 1";
+
+        let mut springs = BitSprings::from_str(line).unwrap();
+        springs.unfold(5);
+        assert_eq!(format!("{springs:?}"), ".#?.#?.#?.#?.#");
+    }
+
+    #[test]
+    fn test_unfold_example_1_groups() {
+        let line = ".# 1";
+
+        let mut groups = GroupSprings::from_str(line).unwrap();
+        groups.unfold(5);
+        assert_eq!(format!("{groups:?}"), "1,1,1,1,1");
+    }
+
+    #[test]
+    fn test_unfold_example_2_springs() {
+        let line = "???.### 1,1,3";
+
+        let mut springs = BitSprings::from_str(line).unwrap();
+        springs.unfold(5);
+        assert_eq!(
+            format!("{springs:?}"),
+            "???.###????.###????.###????.###????.###"
+        );
+    }
+
+    #[test]
+    fn test_unfold_example2_groups() {
+        let line = "???.### 1,1,3";
+
+        let mut groups = GroupSprings::from_str(line).unwrap();
+        groups.unfold(5);
+        assert_eq!(format!("{groups:?}"), "1,1,3,1,1,3,1,1,3,1,1,3,1,1,3");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_unfold_springs_panics_if_too_long() {
+        let line = "...............";
+
+        let mut groups = GroupSprings::from_str(line).unwrap();
+        groups.unfold(8);
+    }
 }
--- day12/src/part2.rs
+++ day12/src/part2.rs
@@ -93,45 +93,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_unfold_example_1_springs() {
-        let line = ".# 1";
-
-        let mut springs = BitSprings::from_str(line).unwrap();
-        springs.unfold(5);
-        assert_eq!(format!("{springs:?}"), ".#?.#?.#?.#?.#");
-    }
-
-    #[test]
-    fn test_unfold_example_1_groups() {
-        let line = ".# 1";
-
-        let mut groups = GroupSprings::from_str(line).unwrap();
-        groups.unfold(5);
-        assert_eq!(format!("{groups:?}"), "1,1,1,1,1");
-    }
-
-    #[test]
-    fn test_unfold_example_2_springs() {
-        let line = "???.### 1,1,3";
-
-        let mut springs = BitSprings::from_str(line).unwrap();
-        springs.unfold(5);
-        assert_eq!(
-            format!("{springs:?}"),
-            "???.###????.###????.###????.###????.###"
-        );
-    }
-
-    #[test]
-    fn test_unfold_example2_groups() {
-        let line = "???.### 1,1,3";
-
-        let mut groups = GroupSprings::from_str(line).unwrap();
-        groups.unfold(5);
-        assert_eq!(format!("{groups:?}"), "1,1,3,1,1,3,1,1,3,1,1,3,1,1,3");
-    }
-
-    #[test]
     fn test_part2_example1() {
         let line = "???.### 1,1,3";
         assert_eq!(count_per_line_with_unfolding(line), 1);
@@ -165,14 +126,5 @@ mod tests {
     fn test_part2_example6() {
         let line = "?###???????? 3,2,1";
         assert_eq!(count_per_line_with_unfolding(line), 506250);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_unfold_springs_panics_if_too_long() {
-        let line = "...............";
-
-        let mut groups = GroupSprings::from_str(line).unwrap();
-        groups.unfold(8);
     }
 }
