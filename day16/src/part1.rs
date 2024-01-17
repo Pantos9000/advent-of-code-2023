e507302780629ec13c08bf77c34da011a88894c1
Andreas Hampp <andreas.hampp@barco.com>
2024-01-17 20:01:06 +0100

    day16/part1: refactor for part2

--- day16/src/part1.rs
+++ day16/src/part1.rs
@@ -3,10 +3,16 @@ use crate::hamster::QuantumHamster;
 use crate::map::{Coords, Map};
 
 pub fn run(input: &str) -> usize {
-    let mut map = Map::parse(input);
+    let map = Map::parse(input);
     let starting_position = Coords::new(0, 0);
     let starting_direction = Direction::Right;
-    let mut hamsters = vec![QuantumHamster::new(starting_position, starting_direction)];
+    let hamster = QuantumHamster::new(starting_position, starting_direction);
+
+    fire_hamster(map, hamster)
+}
+
+pub fn fire_hamster(mut map: Map, hamster: QuantumHamster) -> usize {
+    let mut hamsters = vec![hamster];
 
     while let Some(mut hamster) = hamsters.pop() {
         if let Some(new_hamster) = hamster.reorient(&map) {
