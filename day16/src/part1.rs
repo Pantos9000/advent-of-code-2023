c344d4e289ed0b626993e0e9a8f8b1c78c01ad17
Andreas Hampp <andreas.hampp@barco.com>
2024-01-17 16:13:27 +0100

    day16/part1: add unittest

--- day16/src/part1.rs
+++ day16/src/part1.rs
@@ -1,3 +1,25 @@
 pub fn run(_input: &str) -> usize {
     0 // TODO
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "\
+        .|...\\....\n\
+        |.-.\\.....\n\
+        .....|-...\n\
+        ........|.\n\
+        ..........\n\
+        .........\n\
+        ..../.\\\\..\n\
+        .-.-/..|..\n\
+        .|....-|.\\\n\
+        ..//.|....";
+
+    #[test]
+    fn test_example() {
+        assert_eq!(run(EXAMPLE), 46);
+    }
+}
