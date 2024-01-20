28bfa6a4fc2a0c56e477fd2a237cebe23e41ef82
Andreas Hampp <andreas.hampp@barco.com>
2024-01-20 08:08:03 +0100

    day17/part2: add (wrong) solution

--- day17/src/part2.rs
+++ day17/src/part2.rs
@@ -1,3 +1,8 @@
-pub fn run(_input: &str) -> usize {
-    0 // TODO
+use crate::part1;
+
+pub fn run(input: &str) -> usize {
+    const MAX_STRAIGHT_WALKS: u8 = 10;
+    const MIN_STRAIGHT_WALKS: u8 = 4;
+
+    part1::hamster_wheel::<MIN_STRAIGHT_WALKS, MAX_STRAIGHT_WALKS>(input)
 }
